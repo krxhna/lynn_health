a465d966310e642a55163c6aeed3fe0b56f4bbcd
krxhna <m.krishnakanth1999@gmail.com>
2022-01-29 07:27:04 +0800

    secodncomit

--- pages/hello.js
+++ pages/hello.js
@@ -1,43 +1,100 @@
 
+
+
 // import styles from '../styles/Home.module.css'
 // import { Button} from 'react-bootstrap';
 
 export default function Home() {
-
-
-    const handleclick= () => {
-        alert("mep");
-    }
-
-
-
-
-
-  return (
-    <div className="flex flex-row">
+    const handleclick = () => {
+      alert("mep");
+    };
+  
+    return (
+      <div className="flex flex-col">
+        <div className="h-20 bg-white flex justify-between">
+          <div className=" text-cyan-700 text-xl font-bold pl-4 pt-5 font-Poppins">
+            lynn health
+          </div>
+  
+          <div className="flex justify-evenly w-3/12">
+            <div className=" text-black  pt-7 pl-4 font-Poppinsv">about</div>
+            <div className=" text-black  pt-7 font-Poppins">invvestment</div>
+            <div className=" text-black pt-7 font-Poppins">contanct</div>
+            <div className=" text-black  pt-7 font-Poppins">learn more</div>
+          </div>
         
-        {/* navbar  */}
-        <div className="bg-slate-900 w-52 h-screen flex flex-col text-center justify-evenly">
-
-            <div className="absolute top-0 text-center px-14">
-                heading
-            </div>
-
-           <div className="f">
-           <div className="text-gray-200 font-mono">Home</div>
-            <div className="text-gray-200 text-b">Dashbord</div>
-           </div>
-            <div className="font-sedwick">Account</div>
-
-          
-
-
         </div>
-        active projects
+  
         
-     
-      
-    </div>
-    
-  );
-}
+        <div className="h-screen bg-white flex ">
+         <div className="hj">
+         <div className="text-black w-5/12 bg-white text-9xl m-32 font-Poppins">
+            watch. learn. grow.
+          </div>
+  
+          {/* email  input  */}
+          <div className="flex ">
+          <input type="text" className="shadow-lg w-3/4 h-20 ml-32 bg-white font-Poppins" placeholder=" search"/>
+          <button className="w-1/5 h-20 shadow-xl bg-green-400 font-Poppins">search</button> 
+         
+          </div>
+          
+          
+          </div>
+          
+          <div className="bg-white flex ml-32">
+          <div className="text-sky-50 w-1/4 bg-violet-500 rounded-xl m-16 ">
+            <img
+              src="https://images.unsplash.com/photo-1488426862026-3ee34a7d66df"
+              className="object-cover h-full w-full rounded-xl hover:-translate-y-1 transition ease-in-out delay-150 "
+            >
+  
+              
+            </img>
+          </div>
+          <div className="transition ease-in-out delay-150 bg-blue-500 m-16 w-1/12 hover:-translate-y-1 hover:w-1/4 hover:bg-indigo-500 duration-300">
+            <img
+              src="https://images.unsplash.com/photo-1488426862026-3ee34a7d66df"
+              className="object-cover h-full w-full rounded-xl"
+            ></img>
+          </div>
+          <div className="transition ease-in-out delay-150 bg-blue-500 m-16 w-1/12 hover:-translate-y-1 hover:w-1/4 hover:bg-indigo-500 duration-300">
+            <img
+              src="https://images.unsplash.com/photo-1488426862026-3ee34a7d66df"
+              className="object-cover h-full w-full rounded-xl"
+            ></img>
+          </div>
+          </div>
+        </div>
+        
+        <div className="bg-orange-50 w-full">.</div>
+        <div className="bg-orange-50 w-full h-36 text-center p-12 text-4xl font-bold font-Poppins">
+  
+          Unlimited acess to teachers 
+  
+        </div>
+  
+        <div className="flex bg-orange-50 justify-between px-96 text-xl font-semibold font-Poppinsv">
+          <div className="a">fksodf</div>
+          <div className="a">fksodf</div>
+          <div className="a">fksodf</div>
+          <div className="a">fksodf</div>
+  
+          <div className="a">fksodf</div>
+          <div className="a">fksodf</div>
+          
+        </div>
+  
+        <div className="px-64 py-7 ml-9 bg-orange-50 gap-6 grid grid-cols-4">
+          <div className="h-40 w-44 bg-orange-300 rounded-lg shadow-lg">1</div>
+          <div className="h-40 w-44 bg-green-300 rounded-lg shadow-lg">2</div>
+          <div className="h-40 w-44 bg-blue-300 0 rounded-lg shadow-lg">3</div>
+          <div className="h-40 w-44 bg-red-300 rounded-lg shadow-lg">4</div>
+          <div className="h-40 w-44 bg-cyan-400 rounded-lg shadow-lg">5</div>
+          <div className="h-40 w-44 bg-cyan-400 rounded-lg shadow-lg">6</div>
+          <div className="h-40 w-44 bg-cyan-400 rounded-lg shadow-lg">7</div>v
+        </div>
+      </div>
+    );
+  }
+  
--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,37 @@
+
 // import styles from '../styles/Home.module.css'
 // import { Button} from 'react-bootstrap';
 
 export default function Home() {
-  const handleclick = () => {
-    alert("mep");
-  };
-
-  
 
 
+  const handleclick= () => {
+      alert("mep");
+  }
 
-  return (
-    <div className="flex flex-row">
-      {/* navbar  */}
-      <div className="bg-slate-900 w-52 h-screen flex flex-col text-center justify-evenly">
-        <div className="absolute top-0 text-center px-14">heading</div>
 
-        <div className="flex flex-col gap-4 ">
-          <div className="text-gray-200 font-Poppins font-semibold hover:text-teal-500 ">
-            Home
-          </div>
-          <div className="text-gray-200 font-Poppins font-semibold  hover:text-teal-500">
-            Dashbord
-          </div>
-          <div className="text-gray-200 font-Poppins font-semibold  hover:text-teal-500">
-            Charts
-          </div>
-          <div className="text-gray-200 font-Poppins font-semibold  hover:text-teal-500">
-            News
-          </div>
-        </div>
-        <div className="text-gray-200 font-Poppins font-semibold">Account</div>
-      </div>
 
-      <div className="h-screen bg-white w-64">
-        <button
-          className="shadow-xl m-2 w-36  h-10 bg-gradient-to-r from-teal-500 to-cyan-500 rounded-md text-cyan-50 font-semibold "
-          onClick={handleclick}
-        >
-          yes mommy
-        </button>
-      </div>
+
+
+return (
+  <div className="flex flex-col ">
+    <div className="bg-white text-green-600 h-40 font-bold font-sedwick text-6xl text-center py-8">
+    Nature green tea 
     </div>
-  );
+    <div className="bg-white h-80 px-24">
+    
+    <img
+              src="https://images.unsplash.com/photo-1488426862026-3ee34a7d66df"
+              className="object-cover h-full w-full rounded-2xl shadow-xl "
+            >
+  
+              
+            </img>
+    </div>
+
+      
+      
+  </div>
+  
+);
 }
